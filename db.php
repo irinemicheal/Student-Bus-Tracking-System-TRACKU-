10cea9e360a4871d234a8e0f286a44d54635cb2c
Irine Micheal <113131195+irinemicheal@users.noreply.github.com>
2026-05-08 16:17:18 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,10 @@
 <?php
-$host = "localhost";
-$user = "root";       
-$pass = "";         
-$db   = "studentbustracking";  
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
 
 $mysqli = new mysqli($host, $user, $pass, $db);
-
 if ($mysqli->connect_error) {
     die("Database connection failed: " . $mysqli->connect_error);
 }
